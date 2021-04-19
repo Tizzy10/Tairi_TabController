a78e134cde6be80bdcbb056b3029cb316e803647
TizzJohnson <tizznasty10@gmail.com>
2021-04-19 08:34:00 -0400

    Initial Commit

---
+++ Tairi_TabControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tairi_TabControllers
+//
+//  Created by Tairi on 4/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
